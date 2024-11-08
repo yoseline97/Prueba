f4aee762f6aafc91c1b2659bdf9cbca75a5c069b
VizcainoCristian <ALEXYESTY5511@GMAIL.COM>
2024-11-07 23:34:00 -0600

    CHANGE SUSCR

--- assets/scripts/post/post-client.js
+++ assets/scripts/post/post-client.js
@@ -1,9 +1,6 @@
-// login-client.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
-import { getDatabase, ref, get, child } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 
-// Configuración de Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyAQIa6cMVOojiZZ5zIPkWLGhN5aiMQ0UYs",
     authDomain: "sabi-70919.firebaseapp.com",
@@ -13,38 +10,24 @@ const firebaseConfig = {
     appId: "1:106476736453:web:12142ebafdcdfce7f085ac"
 };
 
-// Inicializar Firebase y la base de datos
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-document.getElementById("loginForm").addEventListener('submit', async function (e) {
+const form = document.getElementById('registerForm');
+
+document.getElementById("adduser").addEventListener('click', function (e) {
     e.preventDefault();
 
-    const email = document.getElementById('text').value;
-    const password = document.getElementById('password').value;
+    const nombre = document.getElementById('email').value;
+    const contraseña = document.getElementById('password').value;
 
-    try {
-        const dbRef = ref(db);
-        const snapshot = await get(child(dbRef, `clients/${text}`));
-
-        if (snapshot.exists()) {
-            const userData = snapshot.val();
-
-            if (userData.CONTRA === password) {
-                alert("Inicio de sesión exitoso");
-
-                // Guardar el email en sessionStorage para usarlo en profile.html
-                sessionStorage.setItem("userEmail", text);
-
-                // Redirigir a profile.html directamente
-                window.location.href = "index2.html";
-            } else {
-                alert("Contraseña incorrecta");
-            }
-        } else {
-            alert("Usuario no encontrado");
-        }
-    } catch (error) {
-        console.error("Error al obtener datos de Firebase:", error);
-    }
+    set(ref(db, 'clients/' + nombre), {
+        nombre: nombre,
+        CONTRA: contraseña
+    }).then(() => {
+        alert("Registro exitoso, ahora puedes iniciar sesión");
+        window.location.href = "index2.html";  // Redirecciona a index.html
+    }).catch((error) => {
+        console.error("Error al registrar:", error);
+    });
 });
